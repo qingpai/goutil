88051860b701f25adbe2d847f277c52b8ec5469d
caotian <caotian@qq.com>
2024-05-09 10:10:48 +0800

    init

--- util/gin.go
+++ util/gin.go
@@ -2,7 +2,10 @@ package util
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/now"
 	"golang.org/x/exp/slices"
+	"strconv"
+	"time"
 )
 
 func ShouldBindJSONWithFields(c *gin.Context, fields []string) (map[string]any, error) {
@@ -37,4 +40,45 @@ func ShouldBindJSONWithOutFields(c *gin.Context, fields []string) (map[string]an
 	}
 
 	return result, nil
+}
+
+func DefaultQueryInt(c *gin.Context, key string) *int {
+	v := c.DefaultQuery(key, "")
+	if v == "" {
+		return nil
+	}
+
+	if i, err := strconv.Atoi(v); err != nil {
+		return nil
+	} else {
+		return &i
+	}
+}
+
+func DefaultQueryInt64(c *gin.Context, key string) *int64 {
+	v := c.DefaultQuery(key, "")
+	if v == "" {
+		return nil
+	}
+
+	if i, err := strconv.ParseInt(v, 10, 64); err != nil {
+		return nil
+	} else {
+		return &i
+	}
+}
+
+func DefaultDate(c *gin.Context, key string) *time.Time {
+	input := c.DefaultQuery(key, "")
+	if input == "" {
+		return nil
+	}
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	input = input + " 00:00:00"
+
+	if v, err := now.ParseInLocation(loc, input); err != nil {
+		return nil
+	} else {
+		return &v
+	}
 }
